45c749de4a168bf9acf9214a7274766a6bfe9eeb
Guilherme Souza <thekingguiiz2003@gmail.com>
2025-02-11 16:51:03 -0300

    feat: implements the birth field on DTO

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,10 @@
-import { IsEmail, IsString, IsStrongPassword } from 'class-validator'
+import {
+  IsDateString,
+  IsEmail,
+  IsOptional,
+  IsString,
+  IsStrongPassword
+} from 'class-validator'
 
 export class CreateUserDTO {
   @IsString()
@@ -7,8 +13,13 @@ export class CreateUserDTO {
   @IsEmail()
   email: string
 
+  @IsString()
   @IsStrongPassword({
     minLength: 6
   })
   password: string
+
+  @IsOptional()
+  @IsDateString()
+  birthAt: string
 }
