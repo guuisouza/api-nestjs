926796e72629c6efa078580681fb26dfd43d61d6
Guilherme Souza <thekingguiiz2003@gmail.com>
2025-02-19 16:14:38 -0300

    feat: implements Throttler to protect the API against DDoS

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,28 @@ import { AppService } from './app.service'
 import { UserModule } from './user/user.module'
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
+import { APP_GUARD } from '@nestjs/core'
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 100
+      }
+    ]),
     forwardRef(() => AuthModule),
     forwardRef(() => UserModule),
     ConfigModule.forRoot()
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
+  ]
 })
 export class AppModule {}
