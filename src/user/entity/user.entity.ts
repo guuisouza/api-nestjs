cff14cc3e129acc106607878429332a2266e37e6
Guilherme Souza <thekingguiiz2003@gmail.com>
2025-02-27 18:21:44 -0300

    refactor: change user entity

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -1,3 +1,4 @@
+import { Role } from 'src/enums/role.enum'
 import {
   Column,
   CreateDateColumn,
@@ -6,7 +7,9 @@ import {
   UpdateDateColumn
 } from 'typeorm'
 
-@Entity()
+@Entity({
+  name: 'users'
+})
 export class UserEntity {
   @PrimaryGeneratedColumn({
     unsigned: true
@@ -33,7 +36,7 @@ export class UserEntity {
     type: 'date',
     nullable: true
   })
-  birthAt: string
+  birthAt: Date
 
   @CreateDateColumn()
   createdAt: string
@@ -42,7 +45,7 @@ export class UserEntity {
   updatedAt: string
 
   @Column({
-    enum: [1, 2]
+    default: Role.User
   })
   role: number
 }
